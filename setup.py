5c3f828481b6fe30d4f39094e2242fac2d1dadea
Fernando Oliveira <fernando@fholiveira.com>
2014-03-08 23:34:19 -0300

    Fix Manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wando',
-    version='0.1',
+    version='0.1.1',
     url='http://packages.python.org/flask-environments/',
     license='BSD',
     author='Fernando Oliveira',
